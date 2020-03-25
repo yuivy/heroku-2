ed0c527c2c2e1d6c1b88246dd6fc7f0329d29841
yuivy <sy2021x@gmail.com>
2020-03-25 10:20:05 +0000

    fog-aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS"],
-    aws_secret_access_key: ENV["AWS_SECRET"],
-    region: 'ap-northeast-1'
-  }
-
-  # config.fog_directory  = 'rails-photo-123'
-  config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/pictgram-siozawa"
-  config.cache_storage = :fog
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'pictgram-siozawa'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS'],
+      aws_secret_access_key: ENV['AWS_SECRET'],
+      region: ENV['AWS_REASION'],
+      path_style: true
+    }
 end
